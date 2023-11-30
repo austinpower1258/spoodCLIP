9d335b31a817c747ed272e4bcfc6ced60d6e8070
austinpower1258 <austinwang115@gmail.com>
2023-11-30 04:44:26 -0500

    working image search

--- spoodclip/image_search/main.py
+++ spoodclip/image_search/main.py
@@ -4,65 +4,78 @@ from sentence_transformers import SentenceTransformer
 import vecs
 from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
+import requests
+from io import BytesIO
+from supabase_py import create_client, Client
 
 DB_CONNECTION = "postgresql://postgres:postgres@localhost:54322/postgres"
+SUPABASE_URL = 'https://ufykdascyufhwemtsaoq.supabase.co'
+SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmeWtkYXNjeXVmaHdlbXRzYW9xIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MTc5NzExMCwiZXhwIjoyMDA3MzczMTEwfQ.LLq9Je691rgwPkyUl-A4GlkhVrN0JxSuiZvHyqaTYq8'
 
 def seed():
+    # Connect to Supabase
+    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+    
+    # Connect to Vecs
     vx = vecs.create_client(DB_CONNECTION)
 
-    images = vx.create_collection(name="image_vecs", dimension=512)
+    # Create collection if it doesn't exist
+    try:
+        images = vx.create_collection(name="spood_vecs6", dimension=512)
+    except Exception as e:
+        print(f"Collection possibly already exists, error: {e}")
+        images = vx.get_collection(name="spood_vecs6")
+
+    # Pre-trained CLIP model
     model = SentenceTransformer('clip-ViT-B-32')
 
-    img_emb1 = model.encode(Image.open('./images/one.jpg'))
-    img_emb2 = model.encode(Image.open('./images/two.jpg'))
-    img_emb3 = model.encode(Image.open('./images/three.jpg'))
-    img_emb4 = model.encode(Image.open('./images/four.jpg'))
+    # Fetch image URLs from Supabase database
+    result = supabase.table('images').select('image_url').execute()
 
-    images.upsert(
-        [
-            (
-                "one.jpg",      
-                img_emb1,        
-                {"type": "jpg"}  
-            ), (
-                "two.jpg",
-                img_emb2,
-                {"type": "jpg"}
-            ), (
-                "three.jpg",
-                img_emb3,
-                {"type": "jpg"}
-            ), (
-                "four.jpg",
-                img_emb4,
-                {"type": "jpg"}
-            )
-        ]
-    )
+    if 'error' in result and result['error'] is not None:
+        raise Exception(f"Error fetching data: {result['error']}")
+
+    # Extracting the data
+    image_urls = [row['image_url'] for row in result['data']]
+
+    # Supabase storage base URL
+    supabase_storage_url = "https://ufykdascyufhwemtsaoq.supabase.co/storage/v1/object/public/images/"
+
+    # Prepare data for upserting
+    data_for_upsert = []
+    for path in image_urls:
+        full_url = supabase_storage_url + path  # Construct full URL
+        response = requests.get(full_url)
+        
+        if response.status_code == 200:
+            image = Image.open(BytesIO(response.content))
+            img_emb = model.encode(image)
+            data_for_upsert.append((full_url, img_emb, {"type": "jpg"}))
+        else:
+            print(f"Failed to fetch image from {full_url}, status code: {response.status_code}")
+
+    # Upsert data
+    images.upsert(data_for_upsert)
 
     print("Inserted images")
     images.create_index()
     print("Created index")
 
-
 def search():
     vx = vecs.create_client(DB_CONNECTION)
-    images = vx.get_collection(name="image_vecs")
+    images = vx.get_collection(name="spood_vecs6")
 
     model = SentenceTransformer('clip-ViT-B-32')
 
-    query_string = "a bike in front of a red brick wall"
+    query_string = "authentic sea urchin from japan"
     text_emb = model.encode(query_string)
 
+    # Fetch 100 images
     results = images.query(
         text_emb,         
-        limit=1,                         
+        limit=100,                         # Number of images to fetch
         filters={"type": {"$eq": "jpg"}},
     )
-    
-    result = results[0]
-    print(result)
-    plt.title(result)
-    image = mpimg.imread('./images/' + result)
-    plt.imshow(image)
-    plt.show()
+    # Print each image URL
+    for result in results:
+        print("Image URL:", result)
